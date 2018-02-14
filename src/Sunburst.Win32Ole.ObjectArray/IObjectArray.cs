c81b55f021ae69b1bd06bed11f1acea4d1fb5cb1
William Kent <wjk@users.noreply.github.com>
2018-02-14 12:36:56 -0500

    Add Sunburst.Win32Ole.ObjectArray project

---
+++ src/Sunburst.Win32Ole.ObjectArray/IObjectArray.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sunburst.Win32Ole.ObjectArray
+{
+    [ComImport, Guid("92CA9DCD-5622-4bba-A805-5E9F541BD8C9")]
+    public interface IObjectArray
+    {
+        void GetCount(out uint pcObjects);
+        void GetAt(uint index, [In] Guid iid, [Out, MarshalAs(UnmanagedType.IUnknown)] out object ppv);
+    }
+}
---
+++ src/Sunburst.Win32Ole.ObjectArray/IObjectCollection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Sunburst.Win32Ole.ObjectArray
+{
+    [ComImport, Guid("5632b1a4-e38a-400a-928a-d4cd63230295")]
+    public interface IObjectCollection
+    {
+        void AddObject([MarshalAs(UnmanagedType.IUnknown)] object obj);
+        void AddFromArray([In] IObjectArray source);
+        void RemoveObjectAt(uint index);
+        void Clear();
+    }
+}
